61e0df90ae199717d8b5889573839dbfb28d2bed
André Pires <andre.r.o.pires@gmail.com>
2016-04-28 00:16:20 +0100

    begin trip planner

--- app/www/js/ostAPI.js
+++ app/www/js/ostAPI.js
@@ -1,55 +1,17 @@
+var base_url_ost = "https://api.ost.pt/";
 
 angular.module('lapd.ost', ['ngCordova'])
 
-.controller('StopsOSTController', function($scope, $http, $cordovaGeolocation){
-	$scope.poslat = 0;
-	$scope.poslon = 0;
-	$scope.range = 0.5;
+.controller('TripPlannerController', function ($scope, $http) {
+	$scope.getTrip = function() {
+		var url = base_url_ost;
 
-	$scope.hasPosition = false;
-
-	$scope.getPos = function() {
-		$scope.hasPosition = true;
-		var options = {timeout: 10000, enableHighAccuracy: true};
-
-		$cordovaGeolocation.getCurrentPosition(options).then(function(position){
-			$scope.poslat = position.coords.latitude;
-			$scope.poslon = position.coords.longitude;
-		});
-
-	};
-
-
-	$scope.getCloseStops = function() {
-		var url = "https://api.ost.pt/stops/?";
-
-		url += "center=" + $scope.poslon + "%2C" + $scope.poslat;
+		/*url += "center=" + $scope.poslon + "%2C" + $scope.poslat;
 		url += "&range=" + $scope.range;
-		url += "&withroutes=false&key=wOfLniMzlmTPRoUSOLmLWVyyWpnnNotUsisSFTTF";
+		url += "&withroutes=false&key=wOfLniMzlmTPRoUSOLmLWVyyWpnnNotUsisSFTTF";*/
 
 		$http.get(url).success( function(response) {
 			$scope.stops = response.Objects;
 		});
 	};
-
-	$scope.numberPickerObject = {
-    inputValue: $scope.range, //Optional
-    minValue: 0,
-    maxValue: 10,
-    decimalCharacter: '.',  //Optional
-    decimalStep: 0.1,  //Optional
-    format: "DECIMAL",  //Optional - "WHOLE" or "DECIMAL"
-    titleLabel: 'Range (km)',  //Optional
-    setLabel: 'Set',  //Optional
-    closeLabel: 'Close',  //Optional
-    setButtonType: 'button-positive',  //Optional
-    closeButtonType: 'button-stable',  //Optional
-    callback: function (val) {    //Mandatory
-    	//timePickerCallback(val);
-    	$scope.range = val;
-    	$scope.getCloseStops();
-    }
-};
-
 });
-
